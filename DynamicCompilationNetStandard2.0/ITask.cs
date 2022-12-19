ab3448bf39bcf1818718daad097c4826403e9725
Adam Paquette <nfs120@gmail.com>
2022-12-18 22:38:03 -0500

    public

--- DynamicCompilationNetStandard2.0/ITask.cs
+++ DynamicCompilationNetStandard2.0/ITask.cs
@@ -3,7 +3,7 @@ using System.Linq.Expressions;
 
 namespace DynamicCompilationNetStandard2._0
 {
-    internal interface ITask
+    public interface ITask
     {
         void CanRun<T>(Expression<Func<T, bool>> predicate);
         void Run();
